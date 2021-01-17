1d1daf903c329e10a4e7db8b56dee7f27320d199
legion <legion1993@163.com>
2021-01-17 19:00:16 +0800

    Second time to commit for git_test2 project. edit HelloGit2.java

--- src/com/legion/test/HelloGit2.java
+++ src/com/legion/test/HelloGit2.java
@@ -3,7 +3,14 @@ package com.legion.test;
 public class HelloGit2 {
     @Override
     public String toString() {
-        System.out.println(this.getClass().getName()+"=======================");
+        System.out.println(this.getClass().getName()+"===========toString============");
+        System.out.println(this.equals(new HelloGit2()));
         return "toString method of HelloGit2";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println(this.getClass().getName()+"===============equals========");
+        return super.equals(obj);
     }
 }
